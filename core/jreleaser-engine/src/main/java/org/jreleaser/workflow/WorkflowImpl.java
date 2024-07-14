76d1b14cd1eedf39ca3f83aa6aff4044e310420b
Osebhulimen Egbor <os614875@dal.ca>
2024-07-14 19:48:02 -0300

    Fixed long method code smell

--- core/jreleaser-engine/src/main/java/org/jreleaser/workflow/WorkflowImpl.java
+++ core/jreleaser-engine/src/main/java/org/jreleaser/workflow/WorkflowImpl.java
@@ -58,13 +58,186 @@ class WorkflowImpl implements Workflow {
         }
     }
 
-    private void doExecute() {
+    static class ExecutionContext {
         RuntimeException stepException = null;
         Throwable listenerException = null;
         Throwable startSessionException = null;
         Throwable endSessionException = null;
+    }
+
+    private void doExecute() {
+        ExecutionContext executionContext = new ExecutionContext();
+//        RuntimeException stepException = null;
+//        Throwable listenerException = null;
+//        Throwable startSessionException = null;
+//        Throwable endSessionException = null;
 
         Instant start = Instant.now();
+
+        filterLogs();
+
+        HookExecutor hooks = new HookExecutor(context);
+
+        executeHooks(hooks, executionContext);
+
+        if (null == executionContext.startSessionException) {
+            startFireSessionEvent(executionContext);
+        }
+
+        if (null == executionContext.startSessionException) {
+            for (WorkflowItem item : items) {
+                boolean failure = false;
+                try {
+                    context.fireWorkflowEvent(ExecutionEvent.before(item.getCommand().toStep()));
+                } catch (WorkflowListenerException beforeException) {
+                    context.getLogger().error(RB.$("listener.failure", beforeException.getListener().getClass().getName()));
+                    context.getLogger().trace(beforeException);
+                    if (!beforeException.getListener().isContinueOnError()) {
+                        executionContext.listenerException = beforeException.getCause();
+                        break;
+                    }
+                }
+
+                try {
+                    item.invoke(context);
+                } catch (RuntimeException e) {
+                    // terminate
+                    failure = true;
+                    executionContext.stepException = e;
+
+                    try {
+                        context.fireWorkflowEvent(ExecutionEvent.failure(item.getCommand().toStep(), e));
+                        break;
+                    } catch (WorkflowListenerException failureException) {
+                        context.getLogger().error(RB.$("listener.failure", failureException.getListener().getClass().getName()));
+                        context.getLogger().trace(failureException);
+                        if (!failureException.getListener().isContinueOnError()) {
+                            executionContext.listenerException = failureException.getCause();
+                            break;
+                        }
+                    }
+                }
+
+                if (!failure) {
+                    try {
+                        context.fireWorkflowEvent(ExecutionEvent.success(item.getCommand().toStep()));
+                    } catch (WorkflowListenerException afterException) {
+                        context.getLogger().error(RB.$("listener.failure", afterException.getListener().getClass().getName()));
+                        context.getLogger().trace(afterException);
+                        if (!afterException.getListener().isContinueOnError()) {
+                            executionContext.listenerException = afterException.getCause();
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        try {
+            context.fireSessionEndEvent();
+        } catch (WorkflowListenerException e) {
+            context.getLogger().error(RB.$("listener.failure", e.getListener().getClass().getName()));
+            context.getLogger().trace(e);
+            if (!e.getListener().isContinueOnError()) {
+                executionContext.endSessionException = e.getCause();
+            }
+        }
+
+        Instant end = Instant.now();
+
+        double duration = Duration.between(start, end).toMillis() / 1000d;
+
+        context.getLogger().reset();
+        context.report();
+
+        closeUpExecution(executionContext ,duration,  hooks);
+    }
+
+    private void executeHooks(HookExecutor hooks, ExecutionContext executionContext) {
+        try {
+            hooks.executeHooks(ExecutionEvent.before(SESSION));
+        } catch (RuntimeException e) {
+            context.getLogger().error(RB.$("ERROR_hooks_unexpected_error"));
+            context.getLogger().trace(e);
+            executionContext.startSessionException = e;
+        }
+    }
+
+    private void closeUpExecution(ExecutionContext executionContext, double duration,HookExecutor hooks) {
+        if (null != executionContext.startSessionException) {
+            hadleStartSessionnException(duration, executionContext.startSessionException);
+        } else if (null != executionContext.endSessionException) {
+            handleEndSessionException(hooks, executionContext.endSessionException, duration);
+        } else {
+            if (null == executionContext.stepException) {
+                handleStepException(executionContext.listenerException, hooks, duration);
+            } else {
+                try {
+                    hooks.executeHooks(ExecutionEvent.failure(SESSION, executionContext.stepException));
+                } catch (RuntimeException e) {
+                    context.getLogger().error(RB.$("ERROR_hooks_unexpected_error"));
+                    context.getLogger().trace(e);
+                }
+
+                context.getLogger().error(RB.$("workflow.failure"), formatDuration(duration));
+                context.getLogger().trace(executionContext.stepException);
+                throw executionContext.stepException;
+            }
+        }
+    }
+
+    private void startFireSessionEvent(ExecutionContext executionContext) {
+        try {
+            context.fireSessionStartEvent();
+        } catch (WorkflowListenerException e) {
+            context.getLogger().error(RB.$("listener.failure", e.getListener().getClass().getName()));
+            context.getLogger().trace(e);
+            if (!e.getListener().isContinueOnError()) {
+                executionContext.startSessionException = e.getCause();
+            }
+        }
+    }
+
+    private void handleStepException(Throwable listenerException, HookExecutor hooks, double duration) {
+        if (null != listenerException) {
+            if (listenerException instanceof RuntimeException) {
+                throw (RuntimeException) listenerException;
+            } else {
+                throw new JReleaserException(RB.$("ERROR_unexpected_error"), listenerException);
+            }
+        }
+        try {
+            hooks.executeHooks(ExecutionEvent.success(SESSION));
+        } catch (RuntimeException e) {
+            context.getLogger().error(RB.$("ERROR_hooks_unexpected_error"));
+            context.getLogger().trace(e);
+        }
+
+        context.getLogger().info(RB.$("workflow.success"), formatDuration(duration));
+    }
+
+    private void handleEndSessionException(HookExecutor hooks, Throwable endSessionException, double duration) {
+        try {
+            hooks.executeHooks(ExecutionEvent.failure(SESSION, endSessionException));
+        } catch (RuntimeException e) {
+            context.getLogger().error(RB.$("ERROR_hooks_unexpected_error"));
+            context.getLogger().trace(e);
+        }
+
+        hadleStartSessionnException(duration, endSessionException);
+    }
+
+    private void hadleStartSessionnException(double duration, Throwable startSessionException) {
+        context.getLogger().error(RB.$("workflow.failure"), formatDuration(duration));
+        context.getLogger().trace(startSessionException);
+        if (startSessionException instanceof RuntimeException) {
+            throw (RuntimeException) startSessionException;
+        } else {
+            throw new JReleaserException(RB.$("ERROR_unexpected_error"), startSessionException);
+        }
+    }
+
+    private void filterLogs() {
         context.getLogger().info(RB.$("workflow.dryrun"), context.isDryrun());
         logFilters("workflow.included.downloader.types", context.getIncludedDownloaderTypes());
         logFilters("workflow.excluded.downloader.types", context.getExcludedDownloaderTypes());
@@ -88,148 +261,6 @@ class WorkflowImpl implements Workflow {
         logFilters("workflow.excluded.uploader.names", context.getExcludedUploaderNames());
         logFilters("workflow.included.announcers", context.getIncludedAnnouncers());
         logFilters("workflow.excluded.announcers", context.getExcludedAnnouncers());
-
-        HookExecutor hooks = new HookExecutor(context);
-
-        try {
-            hooks.executeHooks(ExecutionEvent.before(SESSION));
-        } catch (RuntimeException e) {
-            context.getLogger().error(RB.$("ERROR_hooks_unexpected_error"));
-            context.getLogger().trace(e);
-            startSessionException = e;
-        }
-
-        if (null == startSessionException) {
-            try {
-                context.fireSessionStartEvent();
-            } catch (WorkflowListenerException e) {
-                context.getLogger().error(RB.$("listener.failure", e.getListener().getClass().getName()));
-                context.getLogger().trace(e);
-                if (!e.getListener().isContinueOnError()) {
-                    startSessionException = e.getCause();
-                }
-            }
-        }
-
-        if (null == startSessionException) {
-            for (WorkflowItem item : items) {
-                boolean failure = false;
-                try {
-                    context.fireWorkflowEvent(ExecutionEvent.before(item.getCommand().toStep()));
-                } catch (WorkflowListenerException beforeException) {
-                    context.getLogger().error(RB.$("listener.failure", beforeException.getListener().getClass().getName()));
-                    context.getLogger().trace(beforeException);
-                    if (!beforeException.getListener().isContinueOnError()) {
-                        listenerException = beforeException.getCause();
-                        break;
-                    }
-                }
-
-                try {
-                    item.invoke(context);
-                } catch (RuntimeException e) {
-                    // terminate
-                    failure = true;
-                    stepException = e;
-
-                    try {
-                        context.fireWorkflowEvent(ExecutionEvent.failure(item.getCommand().toStep(), e));
-                        break;
-                    } catch (WorkflowListenerException failureException) {
-                        context.getLogger().error(RB.$("listener.failure", failureException.getListener().getClass().getName()));
-                        context.getLogger().trace(failureException);
-                        if (!failureException.getListener().isContinueOnError()) {
-                            listenerException = failureException.getCause();
-                            break;
-                        }
-                    }
-                }
-
-                if (!failure) {
-                    try {
-                        context.fireWorkflowEvent(ExecutionEvent.success(item.getCommand().toStep()));
-                    } catch (WorkflowListenerException afterException) {
-                        context.getLogger().error(RB.$("listener.failure", afterException.getListener().getClass().getName()));
-                        context.getLogger().trace(afterException);
-                        if (!afterException.getListener().isContinueOnError()) {
-                            listenerException = afterException.getCause();
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        try {
-            context.fireSessionEndEvent();
-        } catch (WorkflowListenerException e) {
-            context.getLogger().error(RB.$("listener.failure", e.getListener().getClass().getName()));
-            context.getLogger().trace(e);
-            if (!e.getListener().isContinueOnError()) {
-                endSessionException = e.getCause();
-            }
-        }
-
-        Instant end = Instant.now();
-
-        double duration = Duration.between(start, end).toMillis() / 1000d;
-
-        context.getLogger().reset();
-        context.report();
-
-        if (null != startSessionException) {
-            context.getLogger().error(RB.$("workflow.failure"), formatDuration(duration));
-            context.getLogger().trace(startSessionException);
-            if (startSessionException instanceof RuntimeException) {
-                throw (RuntimeException) startSessionException;
-            } else {
-                throw new JReleaserException(RB.$("ERROR_unexpected_error"), startSessionException);
-            }
-        } else if (null != endSessionException) {
-            try {
-                hooks.executeHooks(ExecutionEvent.failure(SESSION, endSessionException));
-            } catch (RuntimeException e) {
-                context.getLogger().error(RB.$("ERROR_hooks_unexpected_error"));
-                context.getLogger().trace(e);
-            }
-
-            context.getLogger().error(RB.$("workflow.failure"), formatDuration(duration));
-            context.getLogger().trace(endSessionException);
-            if (endSessionException instanceof RuntimeException) {
-                throw (RuntimeException) endSessionException;
-            } else {
-                throw new JReleaserException(RB.$("ERROR_unexpected_error"), endSessionException);
-            }
-        } else {
-            if (null == stepException) {
-                if (null != listenerException) {
-                    if (listenerException instanceof RuntimeException) {
-                        throw (RuntimeException) listenerException;
-                    } else {
-                        throw new JReleaserException(RB.$("ERROR_unexpected_error"), listenerException);
-                    }
-                }
-                try {
-                    hooks.executeHooks(ExecutionEvent.success(SESSION));
-                } catch (RuntimeException e) {
-                    context.getLogger().error(RB.$("ERROR_hooks_unexpected_error"));
-                    context.getLogger().trace(e);
-                }
-
-                context.getLogger().info(RB.$("workflow.success"), formatDuration(duration));
-            } else {
-                try {
-                    hooks.executeHooks(ExecutionEvent.failure(SESSION, stepException));
-                } catch (RuntimeException e) {
-                    context.getLogger().error(RB.$("ERROR_hooks_unexpected_error"));
-                    context.getLogger().trace(e);
-                }
-
-                context.getLogger().error(RB.$("workflow.failure"), formatDuration(duration));
-                context.getLogger().trace(stepException);
-                throw stepException;
-            }
-        }
     }
 
     private void logFilters(String key, List<String> input) {
